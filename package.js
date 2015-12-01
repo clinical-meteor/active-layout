b4348b0a941e89731189a30684de4692533cc7a2
Abigail Watson <awatson1978@gmail.com>
2015-11-30 22:58:24 -0800

    bump to 0.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.0.3',
+  version: '0.1.0',
   summary: 'Layout templates with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
