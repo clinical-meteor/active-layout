d672ad648bf6ca72adc7c4fc83c732995a2355d0
Abigail Watson <awatson1978@gmail.com>
2015-11-30 23:00:23 -0800

    update package.js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Layout templates with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
@@ -21,21 +21,16 @@ Package.onUse(function (api) {
   api.addFiles('client/layouts/appLayout/appLayout.js', 'client');
   api.addFiles('client/layouts/appLayout/appLayout.less', 'client');
 
+  api.addFiles('client/layouts/appLayout/navbarFooter.js', 'client');
+  api.addFiles('client/layouts/appLayout/navbarFooter.less', 'client');
+  api.addFiles('client/layouts/appLayout/navbarHeader.js', 'client');
+  api.addFiles('client/layouts/appLayout/navbarHeader.less', 'client');
+
   api.addFiles('client/components/errorPanel/errorPanel.html', 'client');
   api.addFiles('client/components/errorPanel/errorPanel.js', 'client');
   api.addFiles('client/components/errorPanel/errorPanel.less', 'client');
 
-  // api.addFiles('client/components/navbarFooter/navbarFooter.html', 'client');
-  api.addFiles('client/components/navbarFooter/navbarFooter.js', 'client');
-  api.addFiles('client/components/navbarFooter/navbarFooter.less', 'client');
-
-  api.addFiles('client/components/navbarHeader/navbarHeader.html', 'client');
-  api.addFiles('client/components/navbarHeader/navbarHeader.js', 'client');
-  api.addFiles('client/components/navbarHeader/navbarHeader.less', 'client');
-
   api.export('appLayout');
-  api.export('navbarHeader');
-  api.export('navbarFooter');
   api.export('errorPanel');
 
 });
