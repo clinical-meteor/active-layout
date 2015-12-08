49804087205ac9060ab21bd3b08056934a3b569e
Abigail Watson <awatson1978@gmail.com>
2015-12-08 01:49:28 -0800

    added overlay component

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.3.8',
+  version: '0.4.0',
   summary: 'Layout templates with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
@@ -17,16 +17,16 @@ Package.onUse(function (api) {
   api.use('grove:less@0.1.1');
   api.use('clinical:router@2.0.13');
 
-
   api.use('clinical:extended-api@2.1.0');
   api.use('clinical:user-model@1.3.1');
   api.use('clinical:keybindings@1.2.0');
   api.use('clinical:overlays@2.1.8');
 
-
+  api.imply('clinical:router');
   api.imply('clinical:extended-api');
   api.imply('clinical:keybindings');
   api.imply('clinical:overlays');
+  api.imply('clinical:user-model');
 
   api.addFiles('client/template.helpers.js', 'client');
   api.addFiles('client/cardDocks.less', 'client');
@@ -46,9 +46,18 @@ Package.onUse(function (api) {
   api.addFiles('client/components/errorPanel/errorPanel.js', 'client');
   api.addFiles('client/components/errorPanel/errorPanel.less', 'client');
 
+  api.addFiles('client/components/overlay/overlay.html', "client");
+  api.addFiles('client/components/overlay/overlay.js', "client");
+  api.addFiles('client/components/overlay/overlay.less', "client");
+
+  api.addFiles('client/lib/overlay.js', "client");
+
+
   api.export('appLayout');
   api.export('errorPanel');
+  api.export('keybindingsModal');
 
+  api.export('Overlay');
   api.export('Header');
 
 });
@@ -57,5 +66,5 @@ Package.onUse(function (api) {
 Package.onTest(function (api) {
   api.use('tinytest');
   api.use('clinical:active-layout');
-  api.addFiles('active-layout-tests.js');
+  api.addFiles('tests/active-layout-tests.js');
 });
