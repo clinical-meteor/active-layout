d3bc53ec42df5f9a21d64d6ac8ff3752c8f06ab6
Abigail Watson <awatson1978@gmail.com>
2016-01-31 02:13:42 -0800

    swapping doesnt seem to work

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.7.8',
+  version: '0.7.9',
   summary: 'Layout templates with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
