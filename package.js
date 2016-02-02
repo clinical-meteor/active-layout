cf465b2c2db80f50b5b321fc4df620642890ca82
Abigail Watson <awatson1978@gmail.com>
2016-02-01 16:46:22 -0800

    renamed footerActionElements to footer

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.7.12',
+  version: '0.7.13',
   summary: 'Layout templates with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
