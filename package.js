a422dd077da2a11245c320b0425296a856bb4649
Abigail Watson <awatson1978@gmail.com>
2016-02-14 19:04:22 -0800

    updated footer

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.7.23',
+  version: '0.7.24',
   summary: 'Layout templates with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
