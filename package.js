9c70cf2eb6a74ecf1a3e44d2ae40f7e782652806
Abigail Watson <awatson1978@gmail.com>
2016-03-06 20:00:41 -0800

    second panel iframe styling

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.7.38',
+  version: '0.7.39',
   summary: 'Universal layout with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
