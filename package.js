b6897babcd8a995f138ddf92ceaea7e4e4c45bba
Abigail Watson <awatson1978@gmail.com>
2016-04-03 11:47:10 -0700

    bump to use published auto-resizing package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.7.49',
+  version: '0.7.50',
   summary: 'Universal layout with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
