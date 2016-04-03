771d81e255aa637886fed4d8950828ec150c3df6
Abigail Watson <awatson1978@gmail.com>
2016-04-02 21:29:02 -0700

    updated glass-ui dep

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:active-layout',
-  version: '0.7.46',
+  version: '0.7.47',
   summary: 'Universal layout with keybindings and animations.',
   git: 'http://github.com/clinical-meteor/clinical-active-layout',
   documentation: 'README.md'
